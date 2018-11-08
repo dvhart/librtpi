01182e9b3d8d655b41bbb1c55ab9acc2b78eddf9
Darren Hart (VMware) <dvhart@infradead.org>
2018-11-07 20:55:05 -0800

    tst-condpi2.c: Convert to librtpi interfaces
    Update tst-condpi2.c to use librtpi interfaces and add to Makefile.am.
    
    This test case currently hangs.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>

--- tst-condpi2.c
+++ tst-condpi2.c
@@ -22,25 +22,25 @@
 #define _GNU_SOURCE
 #include <error.h>
 #include <errno.h>
-#include <pthread.h>
 #include <sched.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <pthread.h>
 #include <string.h>
 #include <time.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include "rtpi.h"
 
 #define LOW_PRIO  1
 #define MED_PRIO  2
 #define HIGH_PRIO 3
 #define MAIN_PRIO 4
 
-static pthread_cond_t race_var;
-static pthread_mutex_t race_mut;
+static pi_cond_t *race_var;
+static pi_mutex_t *race_mut;
 
-static pthread_cond_t sig1, sig2, sig3;
-static pthread_mutex_t m1, m2, m3;
+static pi_cond_t *sig1, *sig2, *sig3;
+static pi_mutex_t *m1, *m2, *m3;
 
 static volatile unsigned int done = 0;
 
@@ -50,14 +50,14 @@ low_tf (void *p)
   int err;
 
   /* Wait for do_test to start all the threads.  */
-  err = pthread_mutex_lock (&m1);
+  err = pi_mutex_lock (m1);
   if (err != 0)
     error (EXIT_FAILURE, err, "low_tf: failed to lock m1");
-  err = pthread_cond_wait (&sig1, &m1);
+  err = pi_cond_wait (sig1);
   if (err != 0)
     error (EXIT_FAILURE, err, "low_tf: cond_wait failed on sig1");
 
-  err = pthread_mutex_lock (&race_mut);
+  err = pi_mutex_lock (race_mut);
   if (err != 0)
     error (EXIT_FAILURE, err, "low_tf: failed to lock race_mut");
 
@@ -65,21 +65,21 @@ low_tf (void *p)
 
   /* Signal the high_tf that we have the race_mut, it will preempt us until it
    * blocks on race_mut.  */
-  err = pthread_cond_signal (&sig2);
+  err = pi_cond_signal (sig2);
   if (err != 0)
     error (EXIT_FAILURE, err, "low_tf: failed to signal sig2");
 
-  /* pthread_cond_wait() holds the cond_lock when it unlocks race_mut.  high_tf
+  /* pi_cond_wait() holds the cond_lock when it unlocks race_mut.  high_tf
      will preempt us before we can release the cond_lock.  It will signal med_tf
      which will continue to block us after high_tf tries to block on race_var if
      it isn't PTHREAD_PRIO_INHERIT, and the cond_lock will never be released.  */
-  err = pthread_cond_wait (&race_var, &race_mut);
+  err = pi_cond_wait (race_var);
   if (err != 0)
     error (EXIT_FAILURE, err, "low_tf: cond_wait failed on race_var");
 
   puts ("low_tf: done waiting");
 
-  err = pthread_mutex_unlock (&race_mut);
+  err = pi_mutex_unlock (race_mut);
   if (err != 0)
     error (EXIT_FAILURE, err, "low_tf: failed to unlock race_mut");
 
@@ -91,38 +91,38 @@ high_tf (void *p)
 {
   int err;
 
-  err = pthread_mutex_lock (&m2);
+  err = pi_mutex_lock (m2);
   if (err != 0)
     error (EXIT_FAILURE, err, "high_tf: failed to lock m2");
 
   /* Wait for low_tf to take race_mut and signal us.  We will preempt low_tf
    * until we block on race_mut below.  */
-  err = pthread_cond_wait (&sig2, &m2);
+  err = pi_cond_wait (sig2);
   if (err != 0)
     error (EXIT_FAILURE, err, "high_tf: cond_wait failed on sig2");
 
   /* Wait for low_tf to release the lock as it waits on race_var.  */
-  err = pthread_mutex_lock (&race_mut);
+  err = pi_mutex_lock (race_mut);
   if (err != 0)
     error (EXIT_FAILURE, err, "high_tf: failed to lock race_mut");
 
   puts ("high_tf: locked");
 
   /* Signal the med_tf to start spinning.  */
-  err = pthread_cond_signal (&sig3);
+  err = pi_cond_signal (sig3);
   if (err != 0)
     error (EXIT_FAILURE, err, "high_tf: failed to signal sig3");
 
   /* If the race_var isn't PTHREAD_PRIO_INHERIT, we will block on the
      race_var cond_lock waiting for the low_tf to release it in it's
-     pthread_cond_wait(&race_var, &race_mut) call.  */
-  err = pthread_cond_wait (&race_var, &race_mut);
+     pi_cond_wait(&race_var, &race_mut) call.  */
+  err = pi_cond_wait (race_var);
   if (err != 0)
     error (EXIT_FAILURE, err, "high_tf: cond_wait failed on race_var");
 
   puts ("high_tf: done waiting");
 
-  err = pthread_mutex_unlock (&race_mut);
+  err = pi_mutex_unlock (race_mut);
   if (err != 0)
     error (EXIT_FAILURE, err, "high_tf: failed to unlock race_mut");
 
@@ -135,12 +135,12 @@ med_tf (void *p)
 {
   int err;
 
-  err = pthread_mutex_lock (&m3);
+  err = pi_mutex_lock (m3);
   if (err != 0)
     error (EXIT_FAILURE, err, "med_tf: failed to lock m3");
 
   /* Wait for high_tf to signal us.  */
-  err = pthread_cond_wait (&sig3, &m3);
+  err = pi_cond_wait (sig3);
   if (err != 0)
     error (EXIT_FAILURE, err, "med_tf: cond_wait failed on sig3");
 
@@ -162,10 +162,7 @@ do_test (void)
   pthread_t high_thread;
   struct sched_param param;
   pthread_attr_t attr;
-  pthread_mutexattr_t m_attr;
-  pthread_condattr_t c_attr;
   cpu_set_t cset;
-
   int err;
 
 
@@ -181,41 +178,44 @@ do_test (void)
   if (err != 0)
     error (EXIT_FAILURE, err, "parent: failed to set attr schedpolicy");
 
-  err = pthread_condattr_init (&c_attr);
-  if (err != 0)
-    error (EXIT_FAILURE, err, "parent: failed to init condattr");
+  race_mut = pi_mutex_alloc();
+  m1 = pi_mutex_alloc();
+  m2 = pi_mutex_alloc();
+  m3 = pi_mutex_alloc();
 
-  err = pthread_cond_init (&sig1, &c_attr);
-  if (err != 0)
-    error (EXIT_FAILURE, err, "parent: failed to init cond sig1");
-  err = pthread_cond_init (&sig2, &c_attr);
-  if (err != 0)
-    error (EXIT_FAILURE, err, "parent: failed to init cond sig2");
-  err = pthread_cond_init (&sig3, &c_attr);
-  if (err != 0)
-    error (EXIT_FAILURE, err, "parent: failed to init cond sig3");
-  err = pthread_cond_init (&race_var, &c_attr);
-  if (err != 0)
-    error (EXIT_FAILURE, err, "parent: failed to init cond race_var");
+  race_var = pi_cond_alloc();
+  sig1 = pi_cond_alloc();
+  sig2 = pi_cond_alloc();
+  sig3 = pi_cond_alloc();
 
-  err = pthread_mutexattr_init (&m_attr);
-  if (err != 0)
-    error (EXIT_FAILURE, err, "parent: failed to init mutexattr");
-  err = pthread_mutexattr_setprotocol (&m_attr, PTHREAD_PRIO_INHERIT);
-  if (err != 0)
-    error (EXIT_FAILURE, err, "parent: failed to set mutexattr protocol");
-  err = pthread_mutex_init (&m1, &m_attr);
+  err = pi_mutex_init (m1, 0);
   if (err != 0)
     error (EXIT_FAILURE, err, "parent: failed to init mutex m1");
-  err = pthread_mutex_init (&m2, &m_attr);
+  err = pi_mutex_init (m2, 0);
   if (err != 0)
     error (EXIT_FAILURE, err, "parent: failed to init mutex m2");
-  err = pthread_mutex_init (&m3, &m_attr);
+  err = pi_mutex_init (m3, 0);
   if (err != 0)
     error (EXIT_FAILURE, err, "parent: failed to init mutex m3");
-  err = pthread_mutex_init (&race_mut, &m_attr);
+  err = pi_mutex_init (race_mut, 0);
   if (err != 0)
     error (EXIT_FAILURE, err, "parent: failed to init mutex race_mut");
+
+  err = pi_cond_init (sig1, m1, 0);
+  if (err != 0)
+    error (EXIT_FAILURE, err, "parent: failed to init cond sig1");
+
+  err = pi_cond_init (sig2, m2, 0);
+  if (err != 0)
+    error (EXIT_FAILURE, err, "parent: failed to init cond sig2");
+
+  err = pi_cond_init (sig3, m3, 0);
+  if (err != 0)
+    error (EXIT_FAILURE, err, "parent: failed to init cond sig3");
+
+  err = pi_cond_init (race_var, race_mut, 0);
+  if (err != 0)
+    error (EXIT_FAILURE, err, "parent: failed to init cond race_var");
 
   /* Setup scheduling parameters and create threads.  */
   param.sched_priority = MAIN_PRIO;
@@ -255,7 +255,7 @@ do_test (void)
 
   /* Wait for the threads to start and block on their respective condvars.  */
   usleep (1000);
-  err = pthread_cond_signal (&sig1);
+  err = pi_cond_signal (sig1);
   if (err != 0)
     error (EXIT_FAILURE, err, "parent: failed to signal condition");
 
@@ -263,7 +263,7 @@ do_test (void)
      PTHREAD_PRIO_INHERIT, low_tf will not have released the race_var cond_lock
      and neither thread will have waited on race_var.  */
   usleep (1000);
-  err = pthread_cond_broadcast (&race_var);
+  err = pi_cond_broadcast (race_var);
   if (err != 0)
     error (EXIT_FAILURE, err, "parent: failed to broadcast condition");
 
